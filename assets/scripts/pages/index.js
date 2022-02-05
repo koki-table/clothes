be3e49ec1b1bfb216b25c5de1887ee9ed8f6a574
koki_s <76271374+koki_s@users.noreply.github.com>
2022-02-05 15:39:54 +0900

    全体の構築

--- assets/scripts/pages/index.js
+++ assets/scripts/pages/index.js
@@ -1,5 +1,10 @@
 import Vue from "vue";
+import ScrollReveal from "scrollreveal";
+import Rellax from "rellax";
 
+
+// nav
+// eslint-disable-next-line no-unused-vars
 var app = new Vue({
   el: "#app",
   data: {
@@ -19,14 +24,33 @@ let swipeOption = {
 };
 new Swiper(".swiper-container", swipeOption);
 
- 
-let btn = document.querySelector('.open-btn');
-btn.addEventListener('click', function(el) {
-    btn.classList.toggle('active');
-})
+let btn = document.querySelector(".open-btn");
+btn.addEventListener("click", function(el) {
+  btn.classList.toggle("active");
+});
 
 
 
+// fade
+ScrollReveal().reveal(".sec-mv", {
+  duration: 1800, // アニメーションの完了にかかる時間
+  viewFactor: 0.2, // 0~1,どれくらい見えたら実行するか
+  reset: false, // 何回もアニメーション表示するか
+  delay: 500,
+});
 
+ScrollReveal().reveal(".fade", {
+  duration: 1600,
+  reset: false,
+});
 
+// parallax
+var rellax = new Rellax('.rellax', {
+  speed: -2,
+  center: false,
+  wrapper: null,
+  round: true,
+  vertical: true,
+  horizontal: false
+});
 
